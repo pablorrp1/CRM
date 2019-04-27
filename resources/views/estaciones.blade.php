e47639c6e3cc84e221740e4437dfcaac548d7a00
Pablo Roche <pablorrp1@gmail.com>
2019-04-26 23:34:26 -0600

    Cambio en estaciones

--- resources/views/estaciones.blade.php
+++ resources/views/estaciones.blade.php
@@ -18,14 +18,14 @@
             <div class="map-wrap">
                 <center>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d61772.99674767539!2d-90.52766571276655!3d14.609766117248704!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sgt!4v1555909856610!5m2!1ses-419!2sgt"
-                        width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        width="800" height="650" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </center>
             </div>
         </div>
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-9">
+    <div class="col-lg-12">
         <div class="table-responsive table--no-card m-b-30">
             <table class="table table-borderless table-striped table-earning">
                 <thead>
